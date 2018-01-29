9c761b116d76f55c0aa412eea1a1a8fdb8499561
poppabear8883 <servnx@gmail.com>
2018-01-29 12:21:36 -0500

    add join team form

--- resources/views/partials/join/JoinForm.vue
+++ resources/views/partials/join/JoinForm.vue
@@ -10,7 +10,7 @@
 
         <div class="col-md-6 col-sm-12">
             <div class="styled-input">
-                <input type="text" />
+                <input type="text" required />
                 <label>Last Name</label>
             </div>
         </div>
@@ -34,21 +34,21 @@
 
         <div class="col-md-4 col-sm-6">
             <div class="styled-input">
-                <input type="text" />
+                <input type="text" required />
                 <label>State</label>
             </div>
         </div>
 
         <div class="col-md-4 col-sm-6">
             <div class="styled-input">
-                <input type="text" />
+                <input type="text" required />
                 <label>Zip</label>
             </div>
         </div>
 
         <div class="col-xs-12">
             <div class="styled-input wide">
-                <textarea required></textarea>
+                <textarea></textarea>
                 <label>Message</label>
             </div>
         </div>
