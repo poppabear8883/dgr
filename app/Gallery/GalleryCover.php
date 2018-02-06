f8939b30c7c784c86b617878598a6a2230efd947
poppabear8883 <servnx@gmail.com>
2018-02-05 19:46:18 -0500

    Gallery

--- app/Gallery/GalleryCover.php
+++ app/Gallery/GalleryCover.php
@@ -1,15 +1,4 @@
 <?php
-/**
- * NOTICE OF LICENSE
- *
- * UNIT3D is open-sourced software licensed under the GNU General Public License v3.0
- * The details is bundled with this project in the file LICENSE.txt.
- *
- * @project    UNIT3D
- * @license    https://choosealicense.com/licenses/gpl-3.0/  GNU General Public License v3.0
- * @author     poppabear
- * @date       2/3/18
- */
 
 namespace App\Gallery;
 
